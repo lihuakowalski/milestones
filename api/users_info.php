601d890ccc3ae98203f7dc1e298b2b8b8a5ca7d7
Lihua Zhu <>
2018-05-16 09:30:26 -0400

    testing

--- api/users_info.php
+++
@@ -1,9 +0,0 @@
-<?php
-$userInfo = array(
-    array('firstName'=>'Harper', 'lastName'=>'Perez', 'gender'=>'M', 'title'=>'Developer'),
-    array('firstName'=>'Teress', 'lastName'=>'Momoka', 'gender'=>'F', 'title'=>'Designer')
-);
-
-echo json_encode($userInfo);
-
-?>
